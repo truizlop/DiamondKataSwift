f09726a8bcb89eb146244851653c7f37a1aecff0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-12-08 14:13:51 +0100

    Initial Commit

---
+++ DiamondKata/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DiamondKata
+//
+//  Created by Tomás Ruiz López on 8/12/16.
+//  Copyright © 2016 Tomás Ruiz-López. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
